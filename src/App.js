0b240666508ed718315c7a2891ca70b8253ad3a0
Albert Kayhko <alkayhko@edu.lapinamk.fi>
2021-02-07 16:56:30 +0200

    Update App.js
    Fixing the introduction to create react app

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Hello, my name is Valtteri!
+          </p>
+          <p>
+            ;)
+          </p>
         <a
           className="App-link"
           href="https://reactjs.org"
